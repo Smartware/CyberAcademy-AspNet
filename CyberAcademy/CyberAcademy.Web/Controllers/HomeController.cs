a8aea7364bccd3e8b51908a6fefa68ae7204c981
Prolifik Lexzy <pro_lifik@live.com>
2018-04-24 11:15:31 +0100

    [Prolifik]

--- CyberAcademy/CyberAcademy.Web/Controllers/HomeController.cs
+++ CyberAcademy/CyberAcademy.Web/Controllers/HomeController.cs
@@ -10,33 +10,14 @@ namespace CyberAcademy.Web.Controllers
 
     public class HomeController : Controller
     {
-        [HttpPost]
-        public ActionResult GetSearchForm(SearchModel model)
+       public ViewResult Index()
         {
-            TempData["Keyword"] = model;
-            return RedirectToAction("SearchForm");
-        }
-
-        [HttpGet]
-        public ActionResult SearchForm()
-        {
-            if (TempData["Keyword"] == null)
-                return View();
-
-            var word = (SearchModel)TempData["Keyword"];
-            var checkText = word.RememberMe ? "Checked" : "Not Checked";
-            ViewData["Keyword"] = $"{word.Keyword} & Remember Me: {checkText}";
-            return View();
-        }
-        // GET: Home
-        public ActionResult Index()
-        {
-            this.ViewData["Male"] = Url.Content("~/images/profile.png");
-
-            this.ViewData["Female"] = Url.Content("~/images/profile2.png");
-
-            this.ViewBag.Styles = "border: 1px solid green;";
-            return View();
+            var product = new Product()
+            {
+                Name = "5 Alive",
+                Description = "Citrus Flavour."
+            };
+            return View(product);
         }
 
     }
---
+++ CyberAcademy/CyberAcademy.Web/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CyberAcademy.Web.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public Boolean IsExpired { get; set; }
+    }
+}
